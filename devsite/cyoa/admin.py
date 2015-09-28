d79f37474f73f3c66561f4900d5f094ed64be469
Mikey Phipps <michael@giantsource.com>
2015-09-28 16:13:21 -0400

    Trying to get more in list display of Snippet Admin (errors on date time)

--- devsite/cyoa/admin.py
+++ devsite/cyoa/admin.py
@@ -1,24 +1,29 @@
 from django.contrib import admin
+from django.utils import timezone
 from cyoa.models import Story, Snippet, Choice
+from datetime import date
 
 class StoryAdmin(admin.ModelAdmin):
     fieldsets = [
          (None,               {'fields': ['story_title', 'story_beginning_snippet', 'story_author', 'story_year', 'story_descript']}),
     ]
+    list_display = ('story_title', 'story_author', 'story_year', 'story_beginning_snippet')
 
 class ChoiceInline(admin.StackedInline):
-    model = Choice
-    extra = 3
+	model = Choice
+	extra = 3
 
 
 class SnippetAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['snippet_title','snippet_text']}),
-        ('Date', {'fields': ['pub_date']}),
+        ('Date', {'fields': ['pub_date']}), 
         ('Image & Ending Options', {'fields': ['display_title', 'ending','display_image','image']}),
     ]
-    inlines = [ChoiceInline]
+    list_display = ('snippet_text', 'pub_date', 'was_published_recently')
 
+#    inlines = [ChoiceInline]
+    
 
 
 
@@ -27,4 +32,3 @@ admin.site.register(Snippet, SnippetAdmin)
 
 
 
-list_display = ('snippet_text', 'pub_date', 'was_published_recently')
